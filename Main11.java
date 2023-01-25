7dcc79203e20f5491e1da16541740b1f04369645
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-01-25 11:39:37 +0530

    25-01-2023

---
+++ Main11.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main11{
+
+public static void main(String[] args) throws Exception {
+    Scanner sc=new Scanner(System.in);
+    int n1=sc.nextInt();
+    int []one=new int[n1];
+    for(int i=0;i<one.length;i++)
+    one[i]=sc.nextInt();
+    
+    int n2=sc.nextInt();
+    int []two=new int[n2];
+    for(int i=0;i<two.length;i++)
+    two[i]=sc.nextInt();
+
+    int []diff=new int[n2];
+    int i=one.length-1;
+    int j=two.length-1;
+    int k=diff.length-1;
+    int c=0;
+    while(k>=0){
+        int d=two[j]-c;
+        if(i>=0)
+        d=d-one[i];
+        if(d<0){
+            d=d+10;
+            c=1;
+        }else
+        c=0;
+        
+        diff[k]=d;
+        i--;
+        j--;
+        k--;
+    }
+        
+        int dx=0;
+        while(dx<diff.length){
+            if(diff[dx]!=0)
+            break;
+            else
+            dx++;
+        }
+        while(dx<diff.length){
+            System.out.println(diff[dx]);
+            dx++;
+        }
+        sc.close();
+
+    }
+
+}
