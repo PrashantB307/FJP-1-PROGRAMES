e2b950adc58a1252fc5d682598b049df7aa849ac
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-01-28 23:15:51 +0530

    28-1-2023

---
+++ Pat10.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Pat10 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+         int n = sc.nextInt();
+         int i,j;
+         int os=n/2;
+         int is=-1;
+         for( i=1;i<=n;i++){
+            for( j=1;j<=os;j++){
+                System.out.print("\t");
+            }
+            System.out.print("*\t");
+            for( j=1;j<=is;j++)
+            System.out.print("\t");
+        }
+        if(i>1 && i<n){
+            System.out.print("*\t");
+        }
+        if(i<=n/2){
+            os--;
+            is+=2;
+        }
+        else{
+            os++;
+            is-=2;
+        }
+        System.out.println();
+        sc.close();                                                                          
+    }
+    
+}
