7afbb7a24a112c3358d45e462074e0d855e589ae
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-03-11 07:49:53 +0530

    11-3-2023

---
+++ Main52.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main52 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int []arr= new int[n];
+        for(int i=0;i<arr.length;i++)
+        arr[i]=sc.nextInt();
+
+        int max=maxOfArray(arr , 0);
+        System.out.println(max);
+
+        sc.close();
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+        if(idx==arr.length-1)
+        return arr[idx];
+
+        int misa=maxOfArray(arr , idx+1);
+        if(arr[idx]>misa)
+        return arr[idx];
+        else
+        return misa;
+    }
+
+}
