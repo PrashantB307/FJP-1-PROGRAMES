ded1a80a4678995d6255aaa79458f0d1ec2691cd
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-03-14 20:00:30 +0530

    14-3-2023

---
+++ Main65.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main65 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=1;i<=arr.length-1;i++){
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            int val=arr[i];
+            System.out.print(val +" ");
+        }
+
+        sc.close();
+    }
+}
