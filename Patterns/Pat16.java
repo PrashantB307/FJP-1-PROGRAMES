d294b0c3e97edcd0182405df8ec12f22035eb87c
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-03-31 19:16:15 +0530

    31-3-2023

--- Patterns/Pat16.java
+++ Patterns/Pat16.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Pat16 {
     public static void main(String[] args){
-       Scanner sc = new Scanner (System.in);
-            int n =sc.nextInt();
-            int i,j;
-            for(i=0;i<n;i++){
-                int val=1;
-                for(j=0;j<=i;j++){
-                System.out.print(val+"\t");
-                val=val*(i-j)/(j+1);
-                }
-                System.out.println();
+       Scanner scn = new Scanner (System.in);
+        int n =scn.nextInt();
+        int i,j;
+        for(i=0;i<n;i++){
+            int val=1;
+            for(j=0;j<=i;j++){
+            System.out.print(val+"\t");
+            val=val*(i-j)/(j+1);
             }
+             System.out.println();
+        }
     }
 }
